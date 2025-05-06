c3e0511c90d1a825b93e6447bb7a83370a6f44f4
Faizan Haider <hfaizan53@gmail.com>
2025-05-06 12:30:56 +0000

    Layout updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,10 @@
+import { useColorScheme } from '@/hooks/useColorScheme';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import 'react-native-reanimated';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -21,7 +21,7 @@ export default function RootLayout() {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> {/* This will render app/(tabs)/_layout.tsx as the initial route */}
           <Stack.Screen name="+not-found" />
         </Stack>
         <StatusBar style="auto" />
