6deb1ea076db20205b4e62ef8259f374732f4814
Faizan Haider <hfaizan53@gmail.com>
2025-05-11 14:12:12 +0000

    text fixed

--- app/(tabs)/index.test.tsx
+++ app/(tabs)/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
+// import { render, fireEvent } from '@testing-library/react-native';
 import HomeScreen from './index'; // Import the HomeScreen component
 
 describe('HomeScreen', () => {
